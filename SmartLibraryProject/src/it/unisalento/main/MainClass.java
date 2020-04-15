87e52a39603981833bf64148a05f5f8c54897f95
MatPed95 <63642841+MatPed95@users.noreply.github.com>
2020-04-15 17:25:39 +0200

    Create SmartLibraryProject - Matteo

---
+++ SmartLibraryProject/src/it/unisalento/main/MainClass.java
@@ -0,0 +1,26 @@
+package it.unisalento.main;
+
+
+
+//import it.unisalento.;
+
+public class MainClass 
+{
+	public static void main (String[] args)
+	{
+		
+		
+		//apriLoginGui
+		
+		//LoginGUI logingui = new LoginGUI();
+		//logingui.setVisible(true);	
+			
+	}	
+			
+			
+		
+		
+		
+	}
+
+
